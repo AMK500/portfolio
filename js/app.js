60d65b4c5c07dd2919a38710663e05e820f2fe7c
AMK500 <m7017182@gmail.com>
2024-10-09 12:42:19 +0300

    loading

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
+const spinnerWrapperEl = document.querySelector('.loading')
+setTimeout(() => {
+  spinnerWrapperEl.style.display= 'none';
+}, 1000);
 
 var swiper = new Swiper(".mySwiper", {
   effect: "cards",
   grabCursor: true,
 });
 
-const spinnerWrapperEl = document.querySelector('.loading')
 
-setTimeout(() => {
-  spinnerWrapperEl.style.display= 'none';
-}, 1000);
