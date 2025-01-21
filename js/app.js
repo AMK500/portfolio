e81952509d1fc8e2a6fcad85de23d657f4ac764d
AMK500 <m7017182@gmail.com>
2025-01-21 17:48:33 +0200

    me

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+const spinnerWrapperEl = document.querySelector('.loading')
+setTimeout(() => {
+  spinnerWrapperEl.style.display= 'none';
+}, 1000);
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
