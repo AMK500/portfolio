014d90c477b5c8861342067312164e3f497f79da
AMK500 <m7017182@gmail.com>
2025-02-01 23:46:13 +0200

    Hero

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
-
-
-const checkbox = document.getElementById('toggle');
+const checkbox = document.getElementById("toggle");
 
 console.log(checkbox);
 
 // استعادة الحالة من localStorage عند تحميل الصفحة
-window.addEventListener('load', () => {
-  const isChecked = localStorage.getItem('toggle') === 'true';
+window.addEventListener("load", () => {
+  const isChecked = localStorage.getItem("toggle") === "true";
   checkbox.checked = isChecked;
 });
 
 // حفظ الحالة في localStorage عند تغييرها
-checkbox.addEventListener('change', () => {
-  localStorage.setItem('toggle', checkbox.checked);
+checkbox.addEventListener("change", () => {
+  localStorage.setItem("toggle", checkbox.checked);
 });
